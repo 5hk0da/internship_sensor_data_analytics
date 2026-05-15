817c6f855dbb0290c50e478604c3cf1ee29d297d
5hk0da <alexandrshkoda200701@gmail.com>
2026-05-16 00:05:13 +0200

    adding functions

--- main.py
+++ main.py
@@ -5,22 +5,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 #creating class as our common indications for future values
 class indications:
-    def __init__(self,dataframe,sensor_drift,noise,overheating,vibration,temperature):
-        self.dataframe = dataframe
-        self.sensor_drift = sensor_drift
-        self.noise = noise
-        self.overheating = overheating
-        self.vibration = vibration
-        self.temperature = temperature
+    def __init__(self):
+        self.sensor_drift = 0
 #making functions for all values
-    def temperature():
-        pass
-    def overheating():
-        pass
-    def vibration():
-        pass
-    def sensor_drift():
-        pass
-    def noise():
-        pass
+    def temperature(self):
+        temp = rnd.uniform(0,45)
+        print(f'The temperature is:{temp}')
+        if temp < 0 or temp > 45:
+            print(f'The temperature is:{temp},something went wrong')
+
+    def overheating(self):
+        over = rnd.uniform(0.1,0.2)
+        print(f'The overheating is:{over}')
+        if over > 100:
+            print(f'The overheating is:{over},the accident is possible')
+
+    def vibration(self):
+        vibr = rnd.uniform(50,100)
+        print(f'The vibraion is:{vibr}')
+        if vibr
+    def sensor_drift(self):
+        sens = rnd.uniform(0.1,0.5)
+        print(f'The sensor_drift is:{sensor_drift}')
+        if sensor_drift > 0.5:
+            print(f'The drift is:{sensor_drift},the accident is possible')
+
+    def noise(self):
+        noise = rnd.uniform(75,80)
+        print(f'The noise is:{noise}')
+        if noise > 100:
+            print(f'The noise is:{noise}A,the accident is possible')
 
