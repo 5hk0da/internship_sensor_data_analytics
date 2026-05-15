dcc164001e0cdf310a1b5814579d4a5aba21a782
5hk0da <alexandrshkoda200701@gmail.com>
2026-05-15 20:27:06 +0200

    adding classes

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+import random as rnd
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+class indications:
+    def __init__(self,dataframe,sensor_drift,noise,overheating,vibration,temperature):
+        self.dataframe = dataframe
+        self.sensor_drift = sensor_drift
+        self.noise = noise
+        self.overheating = overheating
+        self.vibration = vibration
+        self.temperature = temperature
