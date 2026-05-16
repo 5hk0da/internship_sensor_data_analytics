c2960b863427013dc3d397acf3091c969529be06
5hk0da <alexandrshkoda200701@gmail.com>
2026-05-16 20:32:33 +0200

    adding 16.05.2026

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ class Indications:
     def temperature(self):
         self.temp_wear += rnd.uniform(0.1, 0.5)
         value = self.temp_base + self.temp_wear + rnd.uniform(-1, 1)
-        print(f"Temperature: {value:.2f}°C")
+        print(f"Temperature: {value:.1f}°C")
         if value > 45:
             print("WARNING: OVERHEATING")
 
@@ -33,7 +33,7 @@ class Indications:
     def vibration(self):
         self.vibration_wear += rnd.uniform(0.2, 1)
         value = self.vibration_base + self.vibration_wear + rnd.uniform(-2, 2)
-        print(f"Vibration: {value:.2f}")
+        print(f"Vibration: {value:.1f}")
         if value > 100:
             print("WARNING: CRITICAL VIBRATION")
 
@@ -64,3 +64,30 @@ for i in range(15):
     robot.vibration()
     robot.sensor_drift()
     robot.overheating()
+
+
+#Creating a graph
+
+#creating a table by using datafprame and pandas
+df = pd.DataFrame(robot.data)
+#size of our future grap
+fig, ax = plt.subplots(figsize = (10,9))
+
+#setting our graph
+ax.plot(df["time"], df["temp"], label="Temperature")
+ax.plot(df["time"], df["vibration"], label="Vibration")
+ax.plot(df["time"], df["drift"], label="Drift")
+
+#deleting right and top sides
+for spine in ['top', 'right',]:
+    ax.spines[spine].set_visible(False)
+
+ax.set_title ('Robot analis data',lock = 'left',fontsize = 14,color = 'black',fontweight = 'bold',pad=15)
+ax.set_xlabel('X',fontsize = 10,color = 'black')
+ax.set_ylabel('Y',fontsize = 10,color = 'black')
+
+#grid
+ax.yaxis.grid(True,linestyle='--',color='grey',alpha=0.5,zorder=0)
+
+#illustrating our graph
+plt.show()
